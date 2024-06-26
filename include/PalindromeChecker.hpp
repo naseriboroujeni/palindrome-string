b141eba79fea4982d705d403a67d837be70394d7
naseriboroujeni <naseriboroujeni@gmail.com>
2024-06-26 13:17:15 +0330

    Add method docs

--- include/PalindromeChecker.hpp
+++ include/PalindromeChecker.hpp
@@ -1,18 +1,38 @@
 #ifndef PALINDROME_CHECKER_HPP
 #define PALINDROME_CHECKER_HPP
 
-#include<string>
+#include <string>
 
 using namespace std;
 
 class PalindromeChecker {
 
 private:
-   static string filterAndNormalize(const std::string& text);
-   static bool isPalindrome(const string&  input);
+   /**
+    * Filters out non-alphanumeric characters from the given string and converts the remaining
+    * characters to lowercase.
+    *
+    * @param text The string to be filtered and normalized.
+    * @return A new string containing only lowercase alphanumeric characters from the original string.
+    */
+   static string filterAndNormalize(const std::string &text);
+
+   /**
+    * Checks if the given string is a palindrome.
+    *
+    * @param text The string to be checked.
+    * @return true if the string is a palindrome, false otherwise.
+    */
+   static bool isPalindrome(const string &input);
 
 public:
-   static bool isPalindromeIgnorePunctuationSpaceCase(const string& input);
+   /**
+    * Checks if the given string is a palindrome, ignoring punctuation, spaces, and case.
+    *
+    * @param text The string to be checked.
+    * @return true if the string is a palindrome, false otherwise.
+    */
+   static bool isPalindromeIgnorePunctuationSpaceCase(const string &input);
 };
 
 #endif
