d1b7378bc4c3056960b0ef1a04e9ad8953068a3f
naseriboroujeni <naseriboroujeni@gmail.com>
2024-06-26 12:06:26 +0330

    Add main method.

---
+++ source/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "PalindromeChecker.hpp"
+
+using namespace std;
+
+int main() {
+   cout << "Enter a string to check if it's a palindrome." << endl;
+   string text;
+   getline(cin, text);
+   cout << (PalindromeChecker::isPalindrome(text) ? "The string is a palindrome." : "The string is not a palindrome.") << endl;
+   return 0;
+}
